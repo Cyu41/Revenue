b7fbdae3f71c819e53c9f2c45df701ab8346f4a0
Cyu41 <yukichen8841@gmail.com>
2023-01-12 11:15:03 +0800

    Update file

--- 營收_yuchun.py
+++ 營收_yuchun.py
@@ -1,4 +1,38 @@
 import pandas as pd
-a = pd.read_excel("2022月營收更新報表_yuchun.xlsx")
+from os import system
+from time import ctime
 
-# hi
+db = pd.read_csv("data_visualize.csv", low_memory=False)
+update = pd.read_excel("2022月營收更新報表_yuchun.xlsm", sheet_name='工作表2', header=6)
+# update
+########################
+#      整理資料        #
+########################
+update = update.iloc[:, 3:7]
+update = update.rename({'【1】年月':'date', '【2】營收發布日':'annouce_day', 
+                        '【3】單月營收(千元)':'rev'}, axis=1)
+update['st_code'] = update['代號 名稱'].astype(str).str[0:4]
+update['st_name'] = update['代號 名稱'].astype(str).str[5:7]
+
+update['annouce_day'] = update['annouce_day'].astype(str)
+update['Year'] = update['annouce_day'].str[0:4]
+update['month'] = update['annouce_day'].str[4:6]
+update['annouce_day'] = update['annouce_day'].str[0:4] + "/" + update['annouce_day'].str[4:6] + "/" + update['annouce_day'].str[6:8]
+update = update[update.annouce_day != 'nan//'].drop('代號 名稱', axis=1)
+
+db = pd.concat([db, update], axis=0).to_csv('db.csv', encoding='utf_8_sig', header=None)
+
+class Auto_push_to_github(object):
+    def get_time(self):
+        return ctime()
+    def create_git_order(self, time, msg):
+        order_arr = ["git add *", "git commit -m" + '"' + 
+                     time +':' + msg + '"' + "git push main master"]
+        for order in order_arr:
+            system(order)
+    def execute(self, msg):
+        dates = self.get_time()
+        self.create_git_order(dates, msg)
+
+upload = Auto_push_to_github()
+upload.execute('db')
