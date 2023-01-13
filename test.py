6f1d725880ce2de84e88169eeee77e8a6a7ac312
Cyu41 <yukichen8841@gmail.com>
2023-01-13 14:40:29 +0800

    Fri Jan 13 14:40:29 2023: Update_file

--- test.py
+++ test.py
@@ -7,13 +7,7 @@ import numpy as np
 from Upload_git import upload
 
 df = pd.read_csv('db.csv', low_memory=False)
-df
 revenue = pd.read_excel('1217_營收.xlsx', sheet_name='RUN1')
-
-stock_info = revenue.iloc[:, 0:3].dropna().rename(
-    {'公司簡稱':'company', 'TSE新產業名':'new_name', 'TEJ子產業名':'minor_name'}, axis=1)
-stock_info['st_code'] = stock_info.company.str[0:4]
-stock_info['st_name'] = stock_info.company.str[5:]
 new_industry_name = revenue['TSE新產業名.1'].dropna()
 minor_industry_name = revenue['TEJ子產業名.1'].dropna()
 
@@ -21,7 +15,9 @@ minor_industry_name = revenue['TEJ子產業名.1'].dropna()
 選擇
 """
 dropdown = new_industry_name[0]
+dropdown
 st_input = '2330'
+
 year = [2018, 2023]
 
 # 
