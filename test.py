648fc1949cd4ff27bdd844f6da737c80f766fda6
Cyu41 <yukichen8841@gmail.com>
2023-01-16 10:59:28 +0800

    Mon Jan 16 10:59:28 2023: Update_file

--- test.py
+++ test.py
@@ -5,9 +5,30 @@ import plotly.figure_factory as ff  # 圖形工廠
 from plotly.subplots import make_subplots  # 繪製子圖
 import numpy as np
 from Upload_git import upload
+from os import system
+import time
+from time import ctime, sleep
+from Upload_git import upload
+import schedule
+import psycopg2
+import psycopg2.extras as extras
+from sqlalchemy import create_engine
 
-df = pd.read_csv('db.csv', low_memory=False)
+# write a df to a PostgreSQL database
+host='database-1.cyn7ldoposru.us-east-1.rds.amazonaws.com'
+port='5432'
+user='Yu'
+password='m#WA12J#'
+database="JQC_Revenue1"
+
+engine = create_engine(
+    'postgresql://{}:{}@{}:{}/{}'.format(
+        user, password, host, port, database), echo=True)
+df = pd.read_sql('tej_revenue', engine);df
+
+# df = pd.read_csv('db.csv', low_memory=False)
 revenue = pd.read_excel('1217_營收.xlsx', sheet_name='RUN1')
+revenue = pd.read_sql('industry', engine)
 new_industry_name = revenue['TSE新產業名.1'].dropna()
 minor_industry_name = revenue['TEJ子產業名.1'].dropna()
 
