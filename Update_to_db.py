36e859a4feff6a5161f74494dcbeef2eeb80f892
Cyu41 <yukichen8841@gmail.com>
2023-02-02 10:05:09 +0800

    update

--- Update_to_db.py
+++ Update_to_db.py
@@ -17,7 +17,7 @@ database="JQC_Revenue1"
 
 # def auto_update_tej_revenue():
 update = pd.read_excel("2022月營收更新報表_yuchun.xlsm", sheet_name='工作表2', header=6)
-update = pd.read_excel('月營收.xlsx')
+# update = pd.read_excel('月營收.xlsx')
 update = update.iloc[:, 3:7]
 update = update.rename({'【1】年月':'rev_period', '【2】營收發布日':'declaration_date', '【3】單月營收(千元)':'rev'}, axis=1)
 update['st_code'] = update['代號 名稱'].astype(str).str[0:4]
@@ -40,12 +40,10 @@ db = pd.read_csv('db.csv', low_memory=False).reset_index(inplace=False, drop=Tru
 db['st_code'] = db['st_code'].astype(str)
 order = db.columns.values.tolist()
 stock_info = db.loc[:, ['st_code', 'st_name', 'new_industry_name', 'minor_industry_name']].drop_duplicates(keep='first')
-update = pd.merge(update, stock_info, on=['st_name','st_code'], how='outer')
+update = pd.merge(update, stock_info, on=['st_name','st_code'], how='inner')
 update = update[order]
 
-pd.merge(update, db, how='outer')
-db
-update.isin(db)
+update[update.rev_period!= 'NaN']
 
 update.to_sql('tej_revenue', engine, if_exists='append')
 print(ctime(), 'updated to the latest revenue')
